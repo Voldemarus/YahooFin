e7d0c4e82cb13aaed05afd7caab6f57b0cf98b32
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2020-06-24 13:29:36 +0300

    Progress bar in loader customised.

--- SmartPortfolio/FinanceAPI/ReferenceLoader/ReferenceLoaderVCViewController.m
+++ SmartPortfolio/FinanceAPI/ReferenceLoader/ReferenceLoaderVCViewController.m
@@ -8,19 +8,37 @@
 
 #import "Utility.h"
 #import "ReferenceLoaderVCViewController.h"
+#import <YLProgressBar.h>
 
 #import "IEXService.h"
 #import "DAO.h"
 
 
+#define LEFT_RED        15  / 255.0            // Componentes for left interval start
+#define LEFT_GREEN        134  / 255.0
+#define LEFT_BLUE        216  / 255.0
+
+#define MEDIUM_RED        121  / 255.0                        // components for end of left interval and start of right
+#define MEDIUM_GREEN    190  / 255.0
+#define MEDIUM_BLUE        127  / 255.0
+
+#define RIGHT_RED        250  / 255.0                        // components for the last border
+#define RIGHT_GREEN        241  / 255.0
+#define RIGHT_BLUE        60  / 255.0
+
+#define EMPTY_FRAME        104                        // frame of the component
+#define EMPTY_BACK        235                    // background color for the empty part
+
 @interface ReferenceLoaderVCViewController () <IEXServiceReferenceDelegate>
 {
     IEXService *srv;
     DAO *dao;
+    YLProgressBar *pBar;
 }
 @property (weak, nonatomic) IBOutlet UIView *infoView;
 @property (weak, nonatomic) IBOutlet UILabel *countLabel;
-@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+@property (weak, nonatomic) IBOutlet UIView *progressBar;
+
 @property (weak, nonatomic) IBOutlet UILabel *operationPrompt;
 @property (weak, nonatomic) IBOutlet UIView *infoPanelView;
 
@@ -43,12 +61,18 @@
     srv = [IEXService sharedInstance];
     srv.referenceDelegate = self;
     self.view.backgroundColor = [UIColor clearColor];
-
+    self.progressBar.backgroundColor = [UIColor clearColor];
     CALayer *l = self.infoView.layer;
     l.backgroundColor = [UIColor colorWithWhite:1.0 alpha:1.0].CGColor;
     l.cornerRadius = self.infoView.frame.size.height * 0.2;
     l.borderWidth = 1.0;
     l.shadowOffset = CGSizeMake(5, 5);
+
+    l = self.progressBar.layer;
+    l.cornerRadius = 2.0;
+    l.borderColor = [UIColor lightGrayColor].CGColor;
+    l.borderWidth = 0.5;
+
     CGRect frame = self.view.frame;
     UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
     UIVisualEffectView *efView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
@@ -57,26 +81,42 @@
     l.shadowColor = [UIColor darkGrayColor].CGColor;
     l.borderColor = [UIColor blueColor].CGColor;
     self.operationPrompt.text = @"";
-    self.progress.progress = 0.0;
-    self.countLabel.text = @"";
-    
+     self.countLabel.text = @"";
+    //
+    // Progress bar setup
+    //
+    // https://github.com/sleonardo/YLProgressBar
+    //
+    CGRect barFrame = self.progressBar.frame;
+    barFrame.origin.x = 0;
+    barFrame.origin.y = 0;
+    pBar = [[YLProgressBar alloc] initWithFrame:barFrame];
+    [self.progressBar addSubview:pBar];
+
+    pBar.type               = YLProgressBarTypeFlat;
+    pBar.backgroundColor = [UIColor whiteColor];
+    //pBar.progressTintColor  = [UIColor blueColor];
+    pBar.progressTintColors = @[
+        [UIColor colorWithRed:LEFT_RED green:LEFT_GREEN blue:LEFT_BLUE alpha:1.0],
+        [UIColor colorWithRed:MEDIUM_RED green:MEDIUM_GREEN blue:MEDIUM_BLUE alpha:1.0],
+        [UIColor colorWithRed:RIGHT_RED green:RIGHT_GREEN blue:RIGHT_BLUE alpha:1.0],
+    ];
+    pBar.trackTintColor = [UIColor clearColor];
+
+    pBar.hideStripes        = YES;
+    pBar.indicatorTextDisplayMode = YLProgressBarIndicatorTextDisplayModeProgress;
+
+    [pBar setProgress:0.0];
+
     NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
     NSArray *requestSignals = @[
         IEXSERVICE_REF_SECTOR_REQUESTED, IEXSERVICE_REF_TAG_REQUESTED,
         IEXSERVICE_REF_STOCK_REQUESTED,
         IEXSERVICE_REF_SHARE_REQUESTED,
     ];
-//    NSArray *receivedSignals = @[
-//        IEXSERVICE_REF_SECTOR_RECEIVED, IEXSERVICE_REF_TAG_RECEIVED,
-//        IEXSERVICE_REF_TICJER_RECEIVED, IEXSERVICE_REF_STOCK_RECEIVED,
-//        IEXSERVICE_REF_SHARE_RECEIVED,
-//    ];
     for (NSString *sigName in requestSignals) {
         [nc addObserver:self selector:@selector(updateOperationPrompt:) name:sigName object:nil];
     }
-//    for (NSString *sigName in receivedSignals) {
-//        [nc addObserver:self selector:@selector(reinitProgressor:) name:sigName object:nil];
-//    }
 }
 
 
@@ -118,7 +158,7 @@
     } else {
         self.operationPrompt.text = notifier;
     }
-    self.progress.progress = 0.0;   // reset progress indicator
+    [pBar setProgress:0.0];   // reset progress indicator
     self.countLabel.text = @"...";
 }
 
@@ -147,7 +187,7 @@
         NSString *stepPrompt = [NSString stringWithFormat:@"%3d/%.0f",(int)step,totalCount];
         dispatch_async(dispatch_get_main_queue(), ^{
             self.countLabel.text = stepPrompt;
-            self.progress.progress = prog;
+            [self->pBar setProgress:prog animated:NO];
         });
          if ([notifier isEqualToString:IEXSERVICE_REF_TAG_RECEIVED]) {
             Tag *tag = [dao tagForData:d];
